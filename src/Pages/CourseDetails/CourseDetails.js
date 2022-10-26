e6eb2f98f13896deac99b4befb8ca8c44bf28bbd
jakiasultana2022 <rokeyasbd@gmail.com>
2022-10-26 20:56:52 +0600

    download pdf

--- src/Pages/CourseDetails/CourseDetails.js
+++ src/Pages/CourseDetails/CourseDetails.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Pdf from "react-to-pdf";
 
 
 const CourseDetails = () => {
-   
+  const ref = React.createRef();
+
 const courseData = useLoaderData();
 const {courseName,img,cost,duration,start,details} = courseData;
     
     return (
 
         <div>
-          <div>
-               <div data-aos="fade-up" data-aos-duration="1900" className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
+          <div ref={ref}>
+               <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
       <div className='flex flex-col max-w-screen-lg overflow-hidden bg-white border rounded shadow-sm lg:flex-row sm:mx-auto'>
         <div className=' lg:w-1/2 mt-10 mb-10 ml-5'>
           <img
@@ -37,16 +39,10 @@ const {courseName,img,cost,duration,start,details} = courseData;
             <p className='inline-flex items-center font-semibold transition-colors duration-200 text-purple-400 hover:text-deep-purple-800'>
            Course Fee: {cost}
             </p>
-            <a >
-              <button
-                type='submit'
-                className='inline-flex items-center g justify-center btn btn-outline btn-primary h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md  hover:bg-purple-700 focus:shadow-outline focus:outline-none'
-              >
-
-               Download PDF
-              </button>
-            </a>
-           
+            
+            <Pdf targetRef={ref} filename="code-example.pdf">
+        {({ toPdf }) => <button className='text-black btn btn-outline btn-primary' onClick={toPdf} >Download Pdf</button>}
+      </Pdf>
           </div>
           <div className='text-center'>
           <button class="btn btn-outline btn-info mt-5">Get Premium Access</button>

9774269865e58f04dc2698791c3d5cd89db68c0f
jakiasultana2022 <rokeyasbd@gmail.com>
2022-10-26 08:22:00 +0600

    name and logo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import './App.css';
 function App() {
   return (
     <div >
-    <button class="btn">Button</button>
-<button class="btn btn-primary">Button</button>
-<button class="btn btn-secondary">Button</button>
-<button class="btn btn-accent">Button</button>
-<button class="btn btn-ghost">Button</button>
-<button class="btn btn-link">Button</button>
+   
     </div>
   );
 }
